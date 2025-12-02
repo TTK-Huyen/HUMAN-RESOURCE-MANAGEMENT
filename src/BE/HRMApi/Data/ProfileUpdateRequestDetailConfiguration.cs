b0e1a77cc111919b1ceeb044a433d78c7538ce09
Applesry <129361139+Applestry@users.noreply.github.com>
2025-12-02 17:21:34 +0700

    delete file du

--- src/BE/HRMApi/Data/ProfileUpdateRequestDetailConfiguration.cs
+++
@@ -1,9 +0,0 @@
-namespace HrSystem.Data;
-
-public class ProfileUpdateRequestConfiguration : IEntityTypeConfiguration<ProfileUpdateRequest>
-{
-    public void Configure(EntityTypeBuilder<ProfileUpdateRequest> builder)
-    {
-        // mapping bảng profile_update_requests
-    }
-}
