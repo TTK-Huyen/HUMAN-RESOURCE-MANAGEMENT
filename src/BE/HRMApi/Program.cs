c0c2b265b7afa0ff0041a11869227ebae342c38e
TuMinz <thuminhvu3001@gmail.com>
2025-12-03 23:11:44 +0700

    refactor(leave-request): clean up API integration logic

--- src/BE/HRMApi/Program.cs
+++ src/BE/HRMApi/Program.cs
@@ -1,8 +1,6 @@
-var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
 using HrmApi.Data;
 using HrmApi.Repositories;
 using HrmApi.Services;
@@ -56,36 +54,31 @@ var app = builder.Build();
 // SEED DATA MẪU
 using (var scope = app.Services.CreateScope())
 {
-    app.MapOpenApi();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    // Đảm bảo DB/migration đã apply
+    db.Database.Migrate();
+
+    // Nếu chưa có employee nào thì seed
+    if (!db.Employees.Any())
+    {
+        db.Employees.AddRange(
+            new Employee { EmployeeCode = "EMP001" },
+            new Employee { EmployeeCode = "EMP002" }
+        );
+
+        db.SaveChanges();
+    }
 }
 
-app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 // 6. (Tuỳ chọn) Https redirection – nếu gây phiền thì comment lại
 // app.UseHttpsRedirection();
-app.UseCors(MyAllowSpecificOrigins);
+app.UseCors(MyAllowSpecificOrigins); // Kết nối FE
 app.UseAuthorization();
 
 // 7. Map controller routes
 app.MapControllers();
->>>>>>> Stashed changes
+
 
 app.Run();
--- src/BE/HRMApi/obj/Debug/net9.0/HRMApi.AssemblyInfo.cs
+++ src/BE/HRMApi/obj/Debug/net9.0/HRMApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a3cc08ac2a820c78e9f8cc652e3d7cc093583da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2e051cd69618eb019d1cf657e2893bef3b0290a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
